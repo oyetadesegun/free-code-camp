f09890f781dc999cfb6a0ac912949d58ecc9a96a
Oyetade Segun <oyetadesegun@gmail.com>
2023-10-31 23:21:17 +0100

    add

--- functional_programming/every.js
+++ functional_programming/every.js
@@ -6,3 +6,13 @@ function checkPositive(arr) {
    })
     // Only change code above this line
   }
+
+
+  function checkPositive(arr) {
+    // Only change code below this line
+  return arr.some((currentValue)=> currentValue > 0)
+  
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]));

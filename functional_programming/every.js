d8f18d823c8f3e50c15cc08e8271f9d56504bde9
Oyetade Segun <oyetadesegun@gmail.com>
2023-10-31 22:32:27 +0100

    every

---
+++ functional_programming/every.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+    // Only change code below this line
+  
+  return arr.every(function(curr){ 
+  return curr > 0;
+   })
+    // Only change code above this line
+  }

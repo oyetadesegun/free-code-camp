9403b7ac652bcfbf68738f28e211f40203c99850
segun oyetade <segun.oyetade@Allianz.local.ng>
2023-11-04 00:50:47 +0100

    rr

--- PigLatin.js
+++ PigLatin.js
@@ -1,9 +1,15 @@
 function translatePigLatin(str) {
     let newWord = '';
-    if (/^[aeiou]/i.test(str)) {
+    const firstVowelIndex = str.search(/[aeiou]/i);
+  
+    if (firstVowelIndex === -1) {
+      // If there are no vowels, simply add "ay" to the end.
+      newWord = `${str}ay`;
+    } else if (firstVowelIndex === 0) {
+      // If the word starts with a vowel, add "way" to the end.
       newWord = `${str}way`;
     } else {
-      const firstVowelIndex = str.search(/[aeiou]/i);
+      // If the word starts with a consonant, move the consonant(s) to the end and add "ay."
       const consonants = str.slice(0, firstVowelIndex);
       const restOfWord = str.slice(firstVowelIndex);
       newWord = `${restOfWord}${consonants}ay`;
@@ -13,7 +19,5 @@ function translatePigLatin(str) {
     return newWord;
   }
   
-  translatePigLatin("consonant");  // Output: "onsonantcay"
-  translatePigLatin("paragraphs");  // Output: "aragraphspay"
-  translatePigLatin("glove");  // Output: "oveglay"
+  translatePigLatin("rhythm"); // Output: "rhythmay"
   
